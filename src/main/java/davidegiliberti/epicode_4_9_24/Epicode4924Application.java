f691204b4cf33e3dd020a6d0972818e875f67cdf
Davide Giliberti <gilo.davide@hotmail.it>
2024-09-04 15:30:26 +0200

    initial commit

---
+++ src/main/java/davidegiliberti/epicode_4_9_24/Epicode4924Application.java
@@ -0,0 +1,13 @@
+package davidegiliberti.epicode_4_9_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epicode4924Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epicode4924Application.class, args);
+	}
+
+}
---
+++ src/test/java/davidegiliberti/epicode_4_9_24/Epicode4924ApplicationTests.java
@@ -0,0 +1,13 @@
+package davidegiliberti.epicode_4_9_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Epicode4924ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
